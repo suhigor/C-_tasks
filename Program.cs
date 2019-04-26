865a778c93450ec47cfa5c02abd78164aae1dc26
Igor Suhockiy <suhigor@gmail.com>
2019-04-26 13:06:35 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -10,28 +11,48 @@ namespace searchColumnInsideFile
         {
             try
             {
-
-                string searchableFolder = @"C:\Users\txt";
-                string columnsToBeSearched = @"C:\Users\columnsToBeSearched.txt";
+                string searchableFolder = @"C:\Users\suhigor\Downloads\дичь\txt";
+                string columnsToBeSearched = @"C:\Users\suhigor\Downloads\дичь\columnsToBeSearched.txt";
+                int counterUsed = 0;
+                int counterUnused = 0;
+                var unusedColumns = new List<string>();
 
                 string[] columns = File.ReadAllLines(columnsToBeSearched);
                 
-                foreach (var f in Directory.EnumerateFiles(searchableFolder, "*.txt", SearchOption.AllDirectories))
+                foreach (string f in Directory.EnumerateFiles(searchableFolder, "*.txt", SearchOption.AllDirectories))
                 {
-                    var filename = Path.GetFileName(f);
+                    string filename = Path.GetFileName(f);
 
-                    foreach (var searchedColumn in columns)
+                    foreach (string searchedColumn in columns)
                     {
-        
                         string textdata = File.ReadAllText(f);
 
                         if (textdata.Contains(searchedColumn))
                         {
-                            Console.WriteLine("В файле "+ filename + " найден столбец: " + searchedColumn);
-                           
+                            Console.WriteLine("В файле " + filename + " найден столбец: " + searchedColumn);
+                            ++counterUsed;
+                        }
+                        else
+                        {
+                            Console.WriteLine("В файле " + filename + " не найден столбец: " + searchedColumn);
+                            ++counterUnused;
+                            unusedColumns.Add(searchedColumn);
                         }
                     }
-                   Console.WriteLine("---------------------------------------------------------------------");
+                    Console.WriteLine("                                                                     ");
+                    Console.WriteLine("Найдено " + counterUsed + " столбцов из процедуры в файле " + filename);
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    Console.WriteLine("Не найдено " + counterUnused + " столбцов из процедуры в файле " + filename);
+                    Console.WriteLine("_____________________________________________________________________");
+                    foreach (string value in unusedColumns)
+                    {
+                        Console.WriteLine(value);
+                    }
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    Console.WriteLine("                                                                     ");
+                    Console.WriteLine("---------------------------------------------------------------------");
+                    counterUsed = 0;
+                    counterUnused = 0;
                 }
 
                 Console.ReadKey();
@@ -39,11 +60,11 @@ namespace searchColumnInsideFile
             }
             catch (UnauthorizedAccessException uAEx)
             {
-                Console.WriteLine(uAEx.Message);
+              Console.WriteLine(uAEx.Message);
             }
             catch (PathTooLongException pathEx)
             {
-                Console.WriteLine(pathEx.Message);
+              Console.WriteLine(pathEx.Message);
             }
         }
     }
